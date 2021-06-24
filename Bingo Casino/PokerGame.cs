7863248ccb21a752cb70cdcad1082bd9b6e16e69
Mo <mohammed.al.rawe@student.stenden.com>
2021-06-24 14:11:21 +0200

    Small things

--- Bingo Casino/PokerGame.cs
+++ Bingo Casino/PokerGame.cs
@@ -1,5 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 public class PokerGame
