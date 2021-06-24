7860d5dd4039a3117b79f47b8411882304e552db
Mo <mohammed.al.rawe@student.stenden.com>
2021-06-24 14:08:06 +0200

    Poker player & poker table classes

---
+++ Bingo Casino/PokerTable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+class PokerTable : Hand
+{
+    public void AddP(StackLayout stack, Grid grid)
+    {
+        AddOneCardForP(Deck.deal(), stack, grid, false, true);
+    }
+    public void RemoveAll()
+    {
+        hand.Clear();
+        cardsRank = 0;
+    }
+}
