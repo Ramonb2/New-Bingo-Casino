f64ab6925a8899d043764b59cad3d66c6fba56b2
Mo <mohammed.al.rawe@student.stenden.com>
2021-06-24 11:51:03 +0200

    Poker Cards Value class added

---
+++ Bingo Casino/PokerCardsValue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+enum PokerCardsValue
+{
+    pair = 1,
+    twoPairs = 2,
+    trinity = 3,
+    straight = 4,
+    flush = 5,
+    fullHouse = 6,
+    Foursome = 7,
+    straightFlush = 8
+}
